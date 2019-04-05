25ceda717d703034f985e6e9e115f87611e03678
WilliamDann <wcdann1@gmail.com>
2019-04-04 20:51:49 -0700

    Inital commit

---
+++ index.js
@@ -0,0 +1,73 @@
+var Discord  = require('discord.js');
+var Imap     = require('imap'),
+    inspect  = require('util').inspect;
+var fs       = require('fs');
+
+
+var config = JSON.parse(fs.readFileSync('config.json'));
+var imap = new Imap({
+    user: config.user,
+    password: config.password,
+    host: config.host,
+    port: config.port,
+    tls: config.tls
+});
+
+const bot = new Discord.Client();
+bot.login(config.token);
+
+function openInbox(callback) {
+    imap.openBox('INBOX', true, callback);
+}
+  
+// Send the newest message to discord
+function sendNewest() {
+    openInbox(function(err, box) {
+        if (err) throw err;
+
+        var f = imap.seq.fetch(box.messages.total + ':*', {
+            id: 1,
+            bodies: ['HEADER.FIELDS (FROM, SUBJECT)', '1'],
+            struct: true
+        })
+
+        f.on('message', (message, index) => {
+            message.on('body', (stream, info) => {
+                var buffer = '', count = 0;
+                var prefix = '(#' + index + ') ';
+
+                stream.on('data', function(chunk) {
+                    count += chunk.length;
+                    buffer += chunk.toString('utf8');
+                    console.log(prefix + 'Body [%s] (%d/%d)', inspect(info.which), count, info.size);
+                });
+
+                stream.once('end', function() {
+                    var channel = bot.channels.get("562812466778865695"); // announcments channel
+                    channel.send(buffer);
+                    console.log(prefix + 'Body [%s] Finished', inspect(info.which));
+                });
+
+            });
+        });
+        
+        f.once('error', function(err) {
+            console.log('Fetch error: ' + err);
+        });
+        f.once('end', function() {
+            console.log('Done fetching all messages!');
+            // imap.end();
+        });
+    });
+
+}
+
+imap.once('ready', function() {
+    imap.on('mail', mail => {
+        sendNewest();
+    });
+
+    sendNewest();
+});
+
+imap.connect();
